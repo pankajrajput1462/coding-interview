8beb817658d191e1ff88e0ecf811049daa038ee5
pankaj <pankajrajput1462@gmail.com>
2018-06-13 09:05:47 +0530

    refactor of old mess

--- src/main/java/patternMatch/PatternMatch.java
+++ src/main/java/patternMatch/PatternMatch.java
@@ -1,5 +1,6 @@
 package patternMatch;
 
+import java.util.Arrays;
 import java.util.concurrent.atomic.AtomicInteger;
 
 /**
@@ -31,6 +32,42 @@ public class PatternMatch {
             }
         }
         return count.get();
+    }
+
+    /**
+     * function that matches input string wild card
+     */
+    public static boolean strMatch(String text, String pattern, int s_len, int p_len) {
+        if (p_len == 0) {
+            return (s_len == 0);
+        }
+        //Initialise table for strong results of sub problems
+        boolean[][] lookup = new boolean[s_len + 1][p_len + 1];
+
+        for (int i = 0; i < s_len + 1; i++) {
+            Arrays.fill(lookup[i], false);
+        }
+        //empty pattern can match with empty string
+        lookup[0][0] = true;
+        // Only * can match with empty string
+        for (int j = 1; j <= p_len; j++) {
+            if (pattern.charAt(j - 1) == '*') {
+                lookup[0][j] = lookup[0][j - 1];
+            }
+        }
+        for (int i = 1; i <= s_len; i++) {
+            for (int j = 1; j <= p_len; j++) {
+                if (pattern.charAt(j - 1) == '*') {
+                    lookup[i][j] = lookup[i][j - 1] || lookup[i - 1][j];
+                } else if (pattern.charAt(j - 1) == '?' || text.charAt(i - 1) == pattern.charAt(j - 1)) {
+                    lookup[i][j] = lookup[i - 1][j - 1];
+                } else {
+                    lookup[i][j] = false;
+                }
+            }
+        }
+
+        return lookup[s_len][p_len];
     }
 
 }
--- src/test/java/patternMatch/PatternMatchTest.java
+++ src/test/java/patternMatch/PatternMatchTest.java
@@ -1,15 +1,59 @@
 package patternMatch;
 
+import org.junit.Before;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class PatternMatchTest {
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
     @Test
-    public void patternSearchTest() {
+    public void patternSearch() {
         String txt = "AABAACAADAABAAABAA";
         String pat = "AABA";
         assertEquals(3, PatternMatch.patternSearch(txt, pat));
     }
 
+    @Test
+    public void strMatch() {
+        String str = "baaabab";
+        String pattern = "*****ba*****ab";
+
+        assertTrue(PatternMatch.strMatch(str, "*****ba*****ab", str.length(),
+                pattern.length()));
+        assertTrue(PatternMatch.strMatch(str, "ba*****ab", str.length(),
+                pattern.length()));
+        assertTrue(PatternMatch.strMatch(str, "ba*ab", str.length(),
+                pattern.length()));
+        assertTrue(PatternMatch.strMatch(str,"a*ab" , str.length(),
+                pattern.length()));
+        assertTrue(PatternMatch.strMatch(str, "a*****ab", str.length(),
+                pattern.length()));
+        assertTrue(PatternMatch.strMatch(str, "ba*ab****", str.length(),
+                pattern.length()));
+        assertTrue(PatternMatch.strMatch(str, "****", str.length(),
+                pattern.length()));
+        assertTrue(PatternMatch.strMatch(str, "*", str.length(),
+                pattern.length()));
+
+        assertTrue(PatternMatch.strMatch(str, "aa?ab", str.length(),
+                pattern.length()));
+
+        assertTrue(PatternMatch.strMatch(str, "b*b", str.length(),
+                pattern.length()));
+
+        assertTrue(PatternMatch.strMatch(str, "a*a", str.length(),
+                pattern.length()));
+
+        assertTrue(PatternMatch.strMatch(str, "baaabab", str.length(),
+                pattern.length()));
+
+        assertTrue(PatternMatch.strMatch(str, "?baaabab", str.length(),
+                pattern.length()));
+
+    }
 }
