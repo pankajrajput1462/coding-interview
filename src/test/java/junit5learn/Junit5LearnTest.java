3deb460d2ebdc8cbfaa1e49588b0210575f4e6d5
pankaj <pankajrajput1462@gmail.com>
2024-01-10 15:55:16 +0530

    Junit annotation

--- src/test/java/junit5learn/Junit5LearnTest.java
+++ src/test/java/junit5learn/Junit5LearnTest.java
@@ -1,19 +1,72 @@
 package junit5learn;
 
-import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-
+import static org.junit.jupiter.api.Assertions.assertTrue;
+//https://devqa.io/junit-5-annotations/
 public class Junit5LearnTest {
+
+    @BeforeAll
+    static void init() {
+        System.out.println("Only run once before all tests");
+    }
+
     @BeforeEach
      void runBeforeEachTest(){
         System.out.println("I am running before each test");
     }
+
+    @BeforeEach
+    void init(TestInfo testInfo) {
+        String callingTest = testInfo.getTestMethod().get().getName();
+        System.out.println(callingTest);
+    }
     @Test
     public void helloJUnit5() {
         assertEquals(10, 5+5);
+    }
+
+    @ParameterizedTest
+    @ValueSource(strings = { "cali", "bali", "dani" })
+    void endsWithI(String str) {
+        assertTrue(str.endsWith("i"));
+    }
+
+    @Test
+    @DisplayName("Custom test name")
+    void testWithDisplayName() {
+    }
+    @Test
+    @DisplayName("Print test name")
+    void printDisplayName(TestInfo testInfo) {
+        System.out.println(testInfo.getDisplayName());
+    }
+
+    @Disabled
+    @RepeatedTest(value = 5, name = "{displayName} {currentRepetition}/{totalRepetitions}")
+    @DisplayName("RepeatingTest")
+    void customDisplayName(RepetitionInfo repInfo, TestInfo testInfo) {
+        int i = 3;
+        System.out.println(testInfo.getDisplayName() +
+                "-->" + repInfo.getCurrentRepetition()
+        );
+
+        assertEquals(repInfo.getCurrentRepetition(), i);
+    }
+
+    @AfterEach
+    void after(TestInfo testInfo) {
+        String callingTest = testInfo.getTestMethod().get().getName();
+        System.out.println(callingTest);
+    }
+
+
+    @AfterAll
+    static void after() {
+        System.out.println("Only run once after all tests");
     }
 
 }
